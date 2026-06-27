ee90f24d15cbb080a5f27aaa369edde95163f0e6
Mohamed Hashi <mhashi03@gmail.com>
2026-06-28 00:11:47 +0300

    changes

--- addons/website_profile/models/gamification_badge.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Orbex. See LICENSE file for full copyright and licensing details.
-
-from orbex import models
-
-
-class GamificationBadge(models.Model):
-    _name = 'gamification.badge'
-    _inherit = ['gamification.badge', 'website.published.mixin']
--- addons/website_sale_mrp/tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# Part of Orbex. See LICENSE file for full copyright and licensing details.
-
-from . import test_website_sale_product_availability
